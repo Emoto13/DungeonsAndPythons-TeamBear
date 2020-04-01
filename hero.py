54973907cd090ea33e3ec7176be05a20f797150a
Emoto13 <emcho_spassov@abv.bg>
2020-04-01 20:13:29 +0300

    Add hero.py

---
+++ hero.py
@@ -0,0 +1,27 @@
+from base_entity import BaseEntity
+
+
+class Hero(BaseEntity):
+    def get_health(self):
+        pass
+
+    def get_mana(self):
+        pass
+
+    def is_alive(self):
+        pass
+
+    def can_cast(self):
+        pass
+
+    def take_damage(self, damage):
+        pass
+
+    def take_healing(self):
+        pass
+
+    def take_mana(self):
+        pass
+
+    def attack(self):
+        pass
