42fd520cbf7568eb07b6a9342bf693512220c9dd
Nikolai Kasev <n.kasev98@gmail.com>
2020-04-01 21:10:00 +0300

    Added spell.py and Spell class

---
+++ spell.py
@@ -0,0 +1,9 @@
+
+class Spell():
+
+	def __init__(self, name: str = 'Spell', damage: float = 30, mana_cost: float = 0, cast_range: float = 0):
+		self.name = name
+		self.damage = damage
+		self.mana_cost = mana_cost
+		self.cast_range = cast_range
+
