aec54952c00e6dca6946459bb9be8b733fa529b7
Emoto13 <emcho_spassov@abv.bg>
2020-04-01 21:20:39 +0300

    Refactor class Spell and class Weapon

--- spell.py
+++ spell.py
@@ -1,9 +1,7 @@
+class Spell:
 
-class Spell():
-
-	def __init__(self, name: str = 'Spell', damage: float = 30, mana_cost: float = 0, cast_range: float = 0):
+	def __init__(self, name: str = 'Spell', damage: float = 0, mana_cost: float = 0, cast_range: float = 0):
 		self.name = name
 		self.damage = damage
 		self.mana_cost = mana_cost
 		self.cast_range = cast_range
-
--- weapon.py
+++ weapon.py
@@ -1,7 +1,5 @@
-
-class Weapon():
+class Weapon:
 
 	def __init__(self, name: str = 'Weapon', damage: float = 0):
 		self.name = name
 		self.damage = damage
-		
