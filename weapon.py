182de345d28ae43d6464017913b08ae8a43251a1
Emoto13 <emcho_spassov@abv.bg>
2020-04-02 18:45:01 +0300

    Add create method

--- weapon.py
+++ weapon.py
@@ -1,5 +1,13 @@
+import random
+
+
 class Weapon:
 
-	def __init__(self, name: str = 'Weapon', damage: int = 0):
-		self.name = name
-		self.damage = damage
+    def __init__(self, name: str = 'Weapon', damage: int = 0):
+        self.name = name
+        self.damage = damage
+
+    @classmethod
+    def create_weapon(cls, name):
+        damage = random.randint(1, 20)
+        return cls(name=name, damage=damage)
