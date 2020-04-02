85ebc1f3e713b7256a53bc6e57ad4b96c155a556
Emoto13 <emcho_spassov@abv.bg>
2020-04-02 18:47:54 +0300

    Add move_hero()

--- dungeon.py
+++ dungeon.py
@@ -1,6 +1,13 @@
-from utils import read_file, add_coordinates, set_coordinates_for_starting_positions_and_treasures, move_is_legal
+from utils import read_file, add_coordinates, set_coordinates_for_starting_positions_and_treasures, move_is_legal, \
+    fight_enemy, collect_treasure, end_game, nothing_happens
 from hero import Hero
 
+
+# TODO CREATE FILES WITH CONSTANTS
+# TODO SPLIT FIGHT_ENEMY AND COLLECT_TREASURE TO SMALLER FUNCTIONS
+# TODO REFACTOR MOVE_HERO TO SMALLER FUNCTIONS
+# TODO WRITE SOME TEST SO MARTO DOESN'T COMPLAIN ABOUT IT
+# TODO UPDATE HERO POSITION ON THE MAP
 
 class Dungeon:
 
@@ -33,12 +40,46 @@ class Dungeon:
         del self.starting_positions[0]
         self.dungeon_map[self.curr_row][self.curr_column] = 'H'
 
+    def move_hero(self, direction):
+        dicts = {
+            'up': (self.curr_row - 1, self.curr_column),
+            'down': (self.curr_row + 1, self.curr_column),
+            'left': (self.curr_row, self.curr_column - 1),
+            'right': (self.curr_row, self.curr_column + 1)
+        }
+
+        row = dicts[direction][0]
+        col = dicts[direction][1]
+
+        if not move_is_legal(self.dungeon_map, row, col):
+            return False
+
+        position = self.dungeon_map[row][col]
+        dict_of_actions = {
+            'E': fight_enemy,
+            'T': collect_treasure,
+            'G': end_game,
+            '.': nothing_happens
+        }
+
+        dict_of_actions[position](self.hero)
+        self.__update_current_row_and_column(row, col)
+        return True
+
+    def __update_current_row_and_column(self, row, col):
+        self.curr_row = row
+        self.curr_column = col
+
+
 def main():
     d = Dungeon('level1.txt')
-    h = Hero()
-    print(d.curr_column)
-    print(d.dungeon_map)
-    print(d.starting_positions)
+    hero = Hero(name='Luster', title='Dracoslayer')
+    d.spawn(hero)
+    d.move_hero('right')
+    d.move_hero('down')
+
+    print(d.curr_row, d.curr_column)
+    print(hero.spell.damage, hero.weapon.damage)
 
 
 if __name__ == '__main__':
