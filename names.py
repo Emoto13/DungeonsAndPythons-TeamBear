69943838c786855f2248ddb29f56ea8aaee53ce3
Nikolai Kasev <n.kasev98@gmail.com>
2020-04-03 09:52:35 +0300

    Created and added constants to separate files

---
+++ names.py
@@ -0,0 +1,19 @@
+WEAPON_NAMES = ('Sword', 
+				'Hammer', 
+				'Mace', 
+				'Boomerang', 
+				'Shuriken', 
+				'Blade', 
+				'Knifes', 
+				'Spear',
+				'Katana'
+				)
+
+
+SPELL_NAMES = ('Avada Kedavra', 
+				'Expecto Patronum', 
+				'Abra Kadabra', 
+				'Crucio', 'Accio', 
+				'Wingardium Leviosa',
+				'Alarte Ascendare',
+				)
