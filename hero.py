0f0e88a029d5e06aeba9972cd899a5136e41112d
Emoto13 <emcho_spassov@abv.bg>
2020-04-03 15:40:20 +0300

    Add create_hero() and attack_by() to Hero

--- hero.py
+++ hero.py
@@ -1,10 +1,14 @@
 from base_entity import BaseEntity
+from verification_mixin import VerificationMixin
 
 
-class Hero(BaseEntity):
+class Hero(BaseEntity, VerificationMixin):
     def __init__(self, name: str = "Hero", title: str = "No title", health: int = 1, mana: int = 0,
                  mana_regeneration_rate: int = 0):
+
         super().__init__(health=health, mana=mana)
+        self.verify_attributes(name, title, mana_regeneration_rate)
+
         self.name = name
         self.title = title
         self.mana_regeneration_rate = mana_regeneration_rate
@@ -20,11 +24,22 @@ class Hero(BaseEntity):
             return self.spell.damage
         return self.weapon.damage
 
+    def attack_by(self, by='spell'):
+        dicts = {'weapon': self.weapon.damage,
+                 'spell': self.spell.damage
+                 }
 
-def main():
-    h = Hero()
-    print(h.attack('alabala'))
+        if by == 'spell':
+            self.reduce_mana()
 
+        return dicts[by]
 
-if __name__ == '__main__':
-    main()
+    @classmethod
+    def create_hero(cls):
+        name = input('Hero name: ')
+        title = input('Hero title: ')
+        health = int(input('Hero health: '))
+        mana = int(input('Hero mana: '))
+        mana_regeneration_rate = int(input('Hero mana regeneration rate: '))
+
+        return cls(name=name, title=title, health=health, mana=mana, mana_regeneration_rate=mana_regeneration_rate)
