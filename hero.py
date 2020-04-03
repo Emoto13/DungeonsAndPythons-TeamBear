e822dccbbdd02a3d2ece87839998409d435b28e7
Emoto13 <emcho_spassov@abv.bg>
2020-04-03 18:04:27 +0300

    Add create_hero() to Hero

--- hero.py
+++ hero.py
@@ -1,10 +1,12 @@
 from base_entity import BaseEntity
+from spell import Spell
 from verification_mixin import VerificationMixin
+from weapon import Weapon
 
 
 class Hero(BaseEntity, VerificationMixin):
     def __init__(self, name: str = "Hero", title: str = "No title", health: int = 1, mana: int = 0,
-                 mana_regeneration_rate: int = 0):
+                 mana_regeneration_rate: int = 0, weapon: Weapon = Weapon(), spell: Spell = Spell()):
 
         super().__init__(health=health, mana=mana)
         self.verify_attributes(name, title, mana_regeneration_rate)
@@ -12,6 +14,8 @@ class Hero(BaseEntity, VerificationMixin):
         self.name = name
         self.title = title
         self.mana_regeneration_rate = mana_regeneration_rate
+        self.weapon = weapon
+        self.spell = spell
 
     def known_as(self):
         return f'{self.name} the {self.title}'
@@ -42,4 +46,25 @@ class Hero(BaseEntity, VerificationMixin):
         mana = int(input('Hero mana: '))
         mana_regeneration_rate = int(input('Hero mana regeneration rate: '))
 
-        return cls(name=name, title=title, health=health, mana=mana, mana_regeneration_rate=mana_regeneration_rate)
+        has_weapon = input(f"Should {name} the {title} have a weapon? (yes/no) ").lower()
+        weapon = Weapon()
+        if has_weapon == 'yes':
+            print('Create your weapon')
+            weapon_name = input('Weapon name: ')
+            weapon_damage = int(input(f'{weapon_name} damage: '))
+            weapon = Weapon(name=weapon_name, damage=weapon_damage)
+
+        has_spell = input(f"Should {name} the {title} have a spell? (yes/no) ").lower()
+        spell = Spell()
+        if has_spell == 'yes':
+            print(f'Create your spell')
+            spell_name = input('Spell name: ')
+            spell_damage = int(input(f'{spell_name} damage: '))
+            spell_mana_cost = int(input(f'Mana cost of the {spell_name}: '))
+            spell_range = int(input(f'Cast range of the {spell_name}: '))
+            spell = Spell(name=spell_name, damage=spell_damage, mana_cost=spell_mana_cost, cast_range=spell_range)
+
+        return cls(name=name, title=title,
+                   health=health,
+                   mana=mana, mana_regeneration_rate=mana_regeneration_rate,
+                   weapon=weapon, spell=spell)
