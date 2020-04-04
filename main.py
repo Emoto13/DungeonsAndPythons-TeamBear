567b6515eb703d876f3ca7d543f11663d428dad6
Nikolai Kasev <n.kasev98@gmail.com>
2020-04-04 19:13:51 +0300

    Refactored user input and user display

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 from hero import Hero
 from dungeon import Dungeon
-from utils import end_game
+from utils import end_game, clear_screen, display_help, check_choice
+from key_input import get_key_input
 
 
 def main():
     hero = Hero.create_hero()
     dungeon = Dungeon('level1.txt')
     dungeon.spawn(hero)
+    display_help()
+    clear_screen()
 
     while not end_game(dungeon) and dungeon.hero.is_alive():
         dungeon.print_map()
-        direction = input(f"In what direction should {hero.known_as()} move? ").lower()
+        print(f"In what direction should {hero.known_as()} move?")
         try:
-            dungeon.move_hero(direction)
+            choice = get_key_input()
+            check_choice(choice, dungeon, hero)
+            clear_screen()
         except Exception as e:
             print(e)
+            input('\nPress Enter to continue...')
+            clear_screen()
+    clear_screen()
 
 
 if __name__ == '__main__':
