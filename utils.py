1dba54445115b9917d2ab8f3d07121879505fb44
Emoto13 <emcho_spassov@abv.bg>
2020-04-05 17:04:14 +0300

    Add utils.py

---
+++ utils.py
@@ -0,0 +1,29 @@
+from display_info import DisplayInfo
+
+
+def clear_screen():
+    print("\033c", end="")
+
+
+def is_direction(choice):
+    directions = ('up', 'down', 'left', 'right')
+    return choice in directions
+
+
+def check_choice(choice, dungeon):
+    if is_direction(choice):
+        dungeon.move_hero(choice)
+        return
+
+    clear_screen()
+    display_info = DisplayInfo(dungeon.hero)
+    display_info.display_info(choice)
+
+
+def end_game(dungeon):
+    row = dungeon.curr_row
+    column = dungeon.curr_column
+    if dungeon.dungeon_map[row][column] == 'G':
+        input('Game over, you won!\n\nPress Enter to leave')
+        return True
+    return False
