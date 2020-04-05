55e0d301859dd367a1e4505fcdd9025e72df45bc
Emoto13 <emcho_spassov@abv.bg>
2020-04-05 17:02:06 +0300

    Refactor prints to print functions

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 from hero import Hero
 from dungeon import Dungeon
-from utils import end_game, clear_screen, display_help, check_choice
 from key_input import get_key_input
+from utils import clear_screen, end_game, check_choice
+from print_helpers import print_ask_direction
 
 
 def main():
     hero = Hero.create_hero()
     dungeon = Dungeon('level1.txt')
     dungeon.spawn(hero)
-    display_help()
+    # TODO add metohd for starting info
     clear_screen()
 
     while not end_game(dungeon) and dungeon.hero.is_alive():
         dungeon.print_map()
-        print(f"In what direction should {hero.known_as()} move?")
+        print_ask_direction(hero)
+
         try:
             choice = get_key_input()
-            check_choice(choice, dungeon, hero)
-            clear_screen()
+            check_choice(choice, dungeon)
         except Exception as e:
             print(e)
             input('\nPress Enter to continue...')
-            clear_screen()
+
+        clear_screen()
     clear_screen()
 
 
