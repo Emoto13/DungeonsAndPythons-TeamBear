3866ce51ddb0af159707a23819ce4d1ae8ba6733
Nikolai Kasev <n.kasev98@gmail.com>
2020-04-05 22:43:14 +0300

    Refactored input to work with normal keys and arrow keys

--- helpers_and_utilities/key_input.py
+++ helpers_and_utilities/key_input.py
@@ -6,27 +6,43 @@ from helpers_and_utilities.verification_mixin import VerificationMixin
 
 class _Getch:
     def __call__(self):
-            fd = sys.stdin.fileno()
-            old_settings = termios.tcgetattr(fd)
-            try:
-                tty.setraw(sys.stdin.fileno())
-                ch = sys.stdin.read(3)
-            finally:
-                termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-            return ch
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+
+def check_arrow_key(key, get_key):
+    if key == '\x1b':
+        new_key = key
+        key = get_key()
+        new_key += key
+        key = get_key()
+        new_key += key
+        return new_key
+    return key
 
 
 def get_key_input():
-    inkey = _Getch()
-    key_combination = inkey()
+    get_key = _Getch()
+    key_combination = get_key()
 
+    key_combination = check_arrow_key(key_combination, get_key)
     dicts = {
         '\x1b[A': 'up',
         '\x1b[B': 'down',
         '\x1b[C': 'right',
         '\x1b[D': 'left',
-        'hlp': 'help',
-        'chr': 'character_info'
+        'w': 'up',
+        's': 'down',
+        'a': 'left',
+        'd': 'right',
+        'h': 'help',
+        'c': 'character_info'
     }
 
     VerificationMixin.verify_command(dicts, key_combination)
@@ -36,8 +52,8 @@ def get_key_input():
 
 
 def main():
-        for i in range(0, 20):
-                print(get_key_input())
+    for i in range(0, 20):
+        print(get_key_input())
 
 
 if __name__ == '__main__':
