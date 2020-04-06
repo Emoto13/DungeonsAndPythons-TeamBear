7df22b6f99d83e178cb69e5257e4b971de85499d
Nikolai Kasev <n.kasev98@gmail.com>
2020-04-06 16:14:19 +0300

    Refactored and added unittests

--- unittests/test_verification_mixin.py
+++ unittests/test_verification_mixin.py
@@ -1,10 +1,26 @@
-import unittest 
+import unittest
+import sys
+
+sys.path.append('.')
+
 from helpers_and_utilities.verification_mixin import VerificationMixin
 
 
 class TestClassVerificationMixin(unittest.TestCase):
 
-    def test_if_verify_value_returns_error_if_given_negative_value(self):
+    def test_if_verify_string_value_raiser_error(self):
+        v = VerificationMixin()
+        err = None
+
+        try:
+            v.verify_string_value('')
+        except Exception as exc:
+            err = exc
+
+        self.assertIsNotNone(err)
+        self.assertEqual(str(err), 'Value cannot be an empty string')
+
+    def test_if_verify_number_value_returns_error_if_given_negative_value(self):
         v = VerificationMixin()
         err = None
 
@@ -14,9 +30,9 @@ class TestClassVerificationMixin(unittest.TestCase):
             err = exc
 
         self.assertIsNotNone(err)
-        self.assertEqual(str(err),"Value should not be negative")
+        self.assertEqual(str(err), "Value should not be negative")
 
-    def test_if_verify_health_returs_error_if_given_negative_health(self):
+    def test_if_verify_health_returs_error_if_given_negative_or_zero_health(self):
         v = VerificationMixin()
         err = None
 
@@ -26,23 +42,36 @@ class TestClassVerificationMixin(unittest.TestCase):
             err = exc
 
         self.assertIsNotNone(err)
-        self.assertEqual(str(err),"Health should be above 0")
+        self.assertEqual(str(err), "Health should be above 0")
 
     def test_if_verify_if_more_than_max_returns_value_when_value_is_less_than_max(self):
         v = VerificationMixin()
 
-        res = v.verify_if_more_than_max(value = 10, max_value = 20)
+        res = v.verify_if_more_than_max(value=10, max_value=20)
         exp = 10
 
-        self.assertEqual(res,exp)
+        self.assertEqual(res, exp)
 
     def test_if_verify_if_more_than_max_returns_max_value_if_value_is_more_than_max(self):
         v = VerificationMixin()
 
-        res = v.verify_if_more_than_max(value = 500, max_value = 20)
+        res = v.verify_if_more_than_max(value=500, max_value=20)
         exp = 20
 
-        self.assertEqual(res,exp)
+        self.assertEqual(res, exp)
+
+    def test_if_verify_command_raises_error_if_given_key_in_a_dictionary_is_not_there(self):
+        dicts = {'one': 1, 'two': 2}
+        key = 'three'
+        err = None
+
+        try:
+            VerificationMixin.verify_command(dicts, key)
+        except Exception as exc:
+            err = exc
+
+        self.assertIsNotNone(err)
+        self.assertEqual(str(err), "No such command. Try again")
 
 
 if __name__ == '__main__':
--- unittests/test_weapon.py
+++ unittests/test_weapon.py
@@ -1,4 +1,8 @@
-import unittest 
+import unittest
+import sys
+
+sys.path.append('.')
+
 from items.weapon import Weapon
 
 
@@ -7,14 +11,16 @@ class TestWeaponClass(unittest.TestCase):
     def test_if_constructor_sets_attributes_correctly(self):
         w = Weapon('Spear', 20)
 
-        self.assertEqual(w.name,'Spear')
+        self.assertEqual(w.name, 'Spear')
         self.assertEqual(w.damage, 20)
 
     def test_if_create_weapon_class_method_creates_a_Weapon_instance(self):
         w = Weapon.create_weapon('Spear')
+        range_damage = range(1, 21)
 
+        self.assertEqual(Weapon, type(w))
         self.assertEqual(w.name, 'Spear')
-        self.assertEqual(Weapon,type(w))
+        self.assertTrue(w.damage in range_damage)
 
 
 if __name__ == '__main__':
