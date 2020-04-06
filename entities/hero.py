a9a591e3918c67d2eb751edc9a0065630ac1edf3
Nikolai Kasev <n.kasev98@gmail.com>
2020-04-06 16:07:58 +0300

    Set hero stats to be static

--- entities/hero.py
+++ entities/hero.py
@@ -9,8 +9,8 @@ from constants.names import WEAPON_NAMES, SPELL_NAMES
 
 
 class Hero(BaseEntity, VerificationMixin):
-    def __init__(self, name: str = "Hero", title: str = "No title", health: int = 1, mana: int = 0,
-                 mana_regeneration_rate: int = 0, weapon: Weapon = Weapon(), spell: Spell = Spell()):
+    def __init__(self, name: str = "Hero", title: str = "No title", health: int = 100, mana: int = 100,
+                 mana_regeneration_rate: int = 5, weapon: Weapon = Weapon(), spell: Spell = Spell()):
         super().__init__(health=health, mana=mana)
         self.verify_attributes(name, title, mana_regeneration_rate)
 
@@ -44,14 +44,9 @@ class Hero(BaseEntity, VerificationMixin):
         # add except
         name = input('Hero name: ')
         title = input('Hero title: ')
-        health = int(input('Hero health: '))
-        mana = int(input('Hero mana: '))
-        mana_regeneration_rate = int(input('Hero mana regeneration rate: '))
 
         weapon = Weapon.create_weapon(random.choice(WEAPON_NAMES))
         spell = Spell.create_spell(random.choice(SPELL_NAMES))
 
         return cls(name=name, title=title,
-                   health=health,
-                   mana=mana, mana_regeneration_rate=mana_regeneration_rate,
                    weapon=weapon, spell=spell)
