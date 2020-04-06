f0a9a9612a8632a129f3dc1f30ca7b6c3cfea613
Nikolai Kasev <n.kasev98@gmail.com>
2020-04-06 16:08:50 +0300

    Refactored and deleted unused fucntions

--- helpers_and_utilities/verification_mixin.py
+++ helpers_and_utilities/verification_mixin.py
@@ -1,29 +1,21 @@
 class VerificationMixin:
 
-    def verify_string_value(self, string):
+    def verify_string_value(self, string: str):
         if not string:
             raise ValueError("Value cannot be an empty string")
 
-    def verify_number_value(self, value):
+    def verify_number_value(self, value: int):
         if value < 0:
             raise ValueError("Value should not be negative")
 
-    def verify_health(self, health):
+    def verify_health(self, health: int):
         if health <= 0:
             raise ValueError("Health should be above 0")
 
-    def verify_if_more_than_max(self, value, max_value):
+    def verify_if_more_than_max(self, value: int, max_value: int):
         if value > max_value:
             value = max_value
         return value
-
-    def verify_number_attributes(self, *attributes):
-        for attribute in attributes:
-            self.verify_number_value(attribute)
-
-    def verify_string_values(self, *attributes):
-        for attribute in attributes:
-            self.verify_string_values(attribute)
 
     def verify_attributes(self, *attributes):
         dicts = {int: self.verify_number_value,
@@ -34,6 +26,6 @@ class VerificationMixin:
             dicts[type(attribute)](attribute)
 
     @staticmethod
-    def verify_command(dicts, command):
+    def verify_command(dicts: dict, command: str):
         if command not in dicts.keys():
             raise ValueError("No such command. Try again")
